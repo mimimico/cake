3aa00462d435f4fa1097d1e6082bd508ae22cbfe
Vladislav Gritsenko <Vladgritsenko@gmail.com>
2013-11-18 15:18:04 +0200

    ftdeploy fix phase5

--- tpl/scripts/like_demo.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-	$('.like').click(function() {
-		if($('.like').hasClass('liked')) {
-			$('.like').removeClass('liked');
-		} else {
-			$('.like').addClass('liked');
-		}
-	});
-});
