074d4f394358ec91d6c41037cf01bc7cac85d517
Andrew Veles <mrveles@icloud.com>
2013-11-20 22:53:51 +0200

    Bug fixes for Add Item page

--- tpl/scripts/like_demo.js
+++ tpl/scripts/like_demo.js
@@ -6,4 +6,13 @@ $(function () {
 			$('.like').addClass('liked');
 		}
 	});
+
+	var highlights = '.item .browse .highlights';
+	$(highlights).click(function() {
+		if($(highlights).hasClass('highlighted')) {
+			$(highlights).html('Highlight item').removeClass('highlighted');
+		} else {
+			$(highlights).html('Item will be highlighted after adding').addClass('highlighted');
+		}
+	});
 });
